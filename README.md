d75911c1e90259027c1f16781956f5daeac6d049
Karthikeyan S <k@kaos.agency>
2019-06-12 00:43:21 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Usaage:
+node index.js <path to src template file> <stage>
+
+Example:
+node index.js ~/Development/EMT/emt-emails/src/test.html dev
---
+++ index.js
@@ -0,0 +1,85 @@
+const path = require('path');
+const fs = require('fs');
+const cheerio = require('cheerio')
+const axios = require('axios');
+const formurlencoded = require('form-urlencoded').default;
+const minify = require('html-minifier').minify;
+const _ = require('lodash');
+
+const ENDPOINT = "https://bootstrap-email.herokuapp.com/";
+
+function printHeader(header) {
+  // console.log(`---------- ${header} ----------`);
+}
+
+async function convertBsEmail() {
+  // set up filename, base dir, extract markup.
+  const file = process.argv[2];
+  const env = process.argv[3];
+  const filename = path.basename(file).split(".")[0];
+
+  const markup = fs.readFileSync(file, 'utf8');
+
+  const renderedFile = path.resolve(path.dirname(file), `../rendered/${filename}.html`);
+  const outputJsonFile = path.resolve(path.dirname(file), `../json/${env ? `${env}/` : ''}${filename}${env ? `-${env}` : ''}.json`);
+
+  // fetch page and set up cookies and token
+  const initialPage = await axios.get(ENDPOINT);
+  printHeader('COOKIE')
+  const tempCookie = initialPage.headers['set-cookie'][0];
+  const cookie = tempCookie.substring(tempCookie.indexOf("=") + 1, tempCookie.indexOf(";"));
+  // console.log(`_bootstrap_email_rails_example_session: ${cookie}`);
+  
+  const $ = cheerio.load(initialPage.data);
+
+  printHeader('BODY')
+  const body = {
+    "utf": "✓",
+    "authenticity_token": $('[name=authenticity_token]').val(),
+    markup,
+    "checkbox1": "on",
+  }
+
+  let renderedPage;
+  try {
+    renderedPage = await axios.request({
+      url: ENDPOINT,
+      method: "post",
+      headers: {
+        "Cookie": `_bootstrap_email_rails_example_session=${cookie};`,
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      data: formurlencoded(body),
+    })
+  } catch (e) {
+    console.log(e)
+    return;
+  }
+
+  const $2 = cheerio.load(renderedPage.data);
+  const outputHtml = minify($2('#responseCodeMirror').val(), {
+    collapseWhitespace: true,
+    removeComments: true,
+    quoteCharacter: "'",
+  }).replace(/\s{2,}/g, ' ')
+  .replace('"', "'");
+  const $3 = cheerio.load(outputHtml);
+  const outputSubject = $3('.preview').text();
+
+  fs.writeFileSync(renderedFile, outputHtml, 'utf8');
+
+  fs.writeFileSync(outputJsonFile, JSON.stringify({
+    "Template": {
+      "TemplateName": `${filename}${env ? `-${env}` : ''}`,
+      "SubjectPart": outputSubject.replace(/ +(?= )/g,''),
+      "TextPart": "Required",
+      "HtmlPart": outputHtml,
+    }
+  }), 'utf8');
+
+  // console.log({ renderedDirectory });
+}
+
+convertBsEmail();
+
+// cheerio.load
